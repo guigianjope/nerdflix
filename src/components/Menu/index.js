a7509925327981240111b973791438af9e1da87d
guigianjope <guigianjope@gmail.com>
2020-08-02 20:44:10 -0300

    Add - Categoria & Video

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,22 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Logo from '../../assets/img/Logo.png';
 import './Menu.css';
 import Button from '../Button';
-//import ButtonLink from './components/ButtonLink';
+// import ButtonLink from './components/ButtonLink';
 
 function Menu() {
-    return (
-        <nav className="Menu">
-            <a href="/">
-              <img className="Logo" src={Logo} alt="NerdFlix Logo"/>
-            </a>
-            <Button as="a" className="ButtonLink" href="/" >
-              Novo Video
-            </Button>
+  return (
+    <nav className="Menu">
+      <Link to="/">
+        <img className="Logo" src={Logo} alt="AluraFlix logo" />
+      </Link>
 
-        </nav>
-    );
-
+      <Button as={Link} className="ButtonLink" to="/cadastro/video">
+        Novo vídeo
+      </Button>
+    </nav>
+  );
 }
 
 export default Menu;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import { Link } from 'react-router-dom';
 
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import CadastroVideo from './pages/cadastro/Video';
+import CadastroCategoria from './pages/cadastro/Categoria';
+
+
+// Colocar um jogo nessa página: https://www.youtube.com/watch?v=jOAU81jdi-c :)
+
+const Pagina404 = () => (
+ <div>
+   <h1>Página 404 - Está não é a página que você procura!!</h1>
+   <Link to="/"> Voltar para HOME</Link>
+</div>
+)
+
+// Usando Roteamento (single page application SPA)
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={Home} exact />
+      <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route path="/cadastro/categoria" component={CadastroCategoria} />
+      <Route component={Pagina404} />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
-
