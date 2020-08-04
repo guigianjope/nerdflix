d968da098fba15a6ce9fabe24f8f249271393818
guigianjope <guigianjope@gmail.com>
2020-08-03 21:16:32 -0300

    Edit Categoria - Add FormField

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { FooterBase } from './styles';
+import Logo from '../../assets/img/Logo.png';
 
 function Footer() {
   return (
     <FooterBase>
-      <a href="https://www.alura.com.br/">
-        <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
+      <a >
+      <img className="Logo" src={Logo} alt="NerdFlix logo" />
       </a>
       <p>
-        Orgulhosamente criado durante a
+        Orgulhosamente criado por Gui Gianjope durante a
         {' '}
         <a href="https://www.alura.com.br/">
           Imersão React da Alura
